4d459f2792c8e5df11376cdfb3b0ec7c08096beb
Erdem Köşk <erdemkosk@gmail.com>
2024-04-16 20:43:28 +0300

    refactoring on quiz

--- app/tabs/(tabs)/quiz.tsx
+++ app/tabs/(tabs)/quiz.tsx
@@ -1,32 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { FlatList, Platform, Vibration } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, Vibration } from 'react-native';
 import {
-  Heading,
-  Text,
   Box,
   Card,
+  Heading,
+  Text,
   VStack,
   Button,
   ButtonText,
-  ScrollView,
 } from "@gluestack-ui/themed";
 import UserTopBar from '@/components/UserTopBar';
 import { getRandomVerse } from '@/logics/dataHandler';
 
 export default function Quiz() {
   const [time, setTime] = useState(new Date());
-  const [questionText, setQuestionText] = useState();
-  const [answer1, setAnswer1] = useState({ text: '', action: 'primary' });
-  const [answer2, setAnswer2] = useState({ text: '', action: 'primary' });
-  const [answer3, setAnswer3] = useState({ text: '', action: 'primary' });
-  const [answer4, setAnswer4] = useState({ text: '', action: 'primary' });
+  const [question, setQuestion] = useState(null);
+  const [answers, setAnswers] = useState([]);
   const [disableButtons, setDisableButtons] = useState(false);
 
-  let correctIndex=0;
-
   useEffect(() => {
-    prepareQuestionAndAnswer()
-
+    prepareQuestionAndAnswers();
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
@@ -35,106 +28,37 @@ export default function Quiz() {
 
   const handlePressIn = (index) => {
     if (!disableButtons) {
-      setDisableButtons(true)
-  
+      setDisableButtons(true);
       Vibration.vibrate(100);
-
-      console.log(disableButtons)
-
-      if (index === correctIndex) {
-        // Doğru cevap
-        if (index === 0) {
-          setAnswer1({
-            ...answer1,
-            action: 'positive'
-          });
-        } else if (index === 1) {
-          setAnswer2({
-            ...answer2,
-            action: 'positive'
-          });
-        } else if (index === 2) {
-          setAnswer3({
-            ...answer3,
-            action: 'positive'
-          });
-        } else if (index === 3) {
-          setAnswer4({
-            ...answer4,
-            action: 'positive'
-          });
-        }
-      } else {
-        // Yanlış cevap
-        if (index === 0) {
-          setAnswer1({
-            ...answer1,
-            action: 'negative'
-          });
-        } else if (index === 1) {
-          setAnswer2({
-            ...answer2,
-            action: 'negative'
-          });
-        } else if (index === 2) {
-          setAnswer3({
-            ...answer3,
-            action: 'negative'
-          });
-        } else if (index === 3) {
-          setAnswer4({
-            ...answer4,
-            action: 'negative'
-          });
-        }
-      }
-      
-
+      const isCorrect = index === question.correctIndex;
+      const updatedAnswers = answers.map((answer, i) => ({
+        ...answer,
+        action: i === index ? (isCorrect ? 'positive' : 'negative') : 'primary',
+      }));
+      setAnswers(updatedAnswers);
       setTimeout(() => {
-        setDisableButtons(false)
-        
-        prepareQuestionAndAnswer();
+        setDisableButtons(false);
+        prepareQuestionAndAnswers();
       }, 1000);
     }
   };
 
-  function prepareQuestionAndAnswer() {
-    const question = getRandomVerse();
-    const questionText = `${question?.verses[0].translation.tr}\n${question?.verses[0].text}`;
-    const verseIds = question?.verses.map(verse => verse.verseId) || [];
-
-    setAnswer1({
-      text: question?.meccanTranslation.tr + ' ' + verseIds[0],
-      action: 'primary'
-    })
-
-    setAnswer2({
-      text: question?.meccanTranslation.tr + ' ' + verseIds[1],
-      action: 'primary'
-    })
-
-    setAnswer3({
-      text: question?.meccanTranslation.tr + ' ' + verseIds[2],
-      action: 'primary'
-    })
-
-    setAnswer4({
-      text: question?.meccanTranslation.tr + ' ' + verseIds[3],
-      action: 'primary'
-    })
-  
-    // Hazırlanan soru metni ve cevap butonlarını setAnswers ile state'e ata
-    setQuestionText(questionText);
-  
-  }
+  const prepareQuestionAndAnswers = () => {
+    const newQuestion = getRandomVerse();
+    const verseText = newQuestion ? `${newQuestion?.verses[0].translation.tr}\n${newQuestion?.verses[0].text}` : '';
+    const verseIds = newQuestion ? newQuestion?.verses.map(verse => verse.verseId) : [];
+    const newAnswers = verseIds.map((verseId, index) => ({
+      text: `${newQuestion?.meccanTranslation.tr} ${verseId}`,
+      action: 'primary',
+    }));
+    setQuestion({ text: verseText, correctIndex: 0 });
+    setAnswers(newAnswers);
+  };
 
   return (
-
     <Box flex={1} my="$8" py="$8" px="$3">
-
       <UserTopBar userName='Erdem Köşk' userTitle='Backend Developer' />
       <VStack py="$1/6">
-
         <Card h="$3/5" justifyContent="center" alignItems="center" size="md" variant="elevated" m="$3">
           <Box position="absolute" justifyContent="center" alignItems="center" borderRadius={100} top={-50} overflow="hidden" borderWidth={2} borderColor="white" width={100} height={100} backgroundColor="black">
             <Text color="white" fontSize="$xl">
@@ -143,30 +67,19 @@ export default function Quiz() {
           </Box>
           <ScrollView>
             <Heading py="$10" mb="$1" size="md">
-
-              {questionText}
+              {question ? question.text : ''}
             </Heading>
           </ScrollView>
           <Text size="sm">Start building your next project in minutes</Text>
         </Card>
         <VStack h="$1/5" space='lg' m="$3">
-        <Button action={answer1.action} disabled={disableButtons}  onPressIn={() => handlePressIn(0)}>
-        <ButtonText>{answer1.text}</ButtonText>
-      </Button>
-      <Button action={answer2.action} disabled={disableButtons}  onPressIn={() => handlePressIn(1)}>
-        <ButtonText>{answer2.text}</ButtonText>
-      </Button>
-      <Button action={answer3.action} disabled={disableButtons}  onPressIn={() => handlePressIn(2)}>
-        <ButtonText>{answer3.text}</ButtonText>
-      </Button>
-      <Button action={answer4.action} disabled={disableButtons}  onPressIn={() => handlePressIn(3)}>
-        <ButtonText>{answer4.text}</ButtonText>
-      </Button>
-
+          {answers.map((answer, index) => (
+            <Button key={index} action={answer.action} disabled={disableButtons} onPressIn={() => handlePressIn(index)}>
+              <ButtonText>{answer.text}</ButtonText>
+            </Button>
+          ))}
         </VStack>
       </VStack>
-
     </Box>
   );
 }
-
